30bfa510cbc0647fad9c71c4224e3813ab0ade35
Shaun <shaun@meshfresh.com>
2019-02-05 17:42:36 -0500

    QA to resolve issues prior to sending site to client, partial

--- partials/stay_nearby.php
+++ partials/stay_nearby.php
@@ -2,16 +2,16 @@
 	<h2 class="section-title stay">Looking to stay nearby?</h2>
 	<div class="m-partners">
 		<div class="row">
-			<div class="columns-4 m-partner">
+			<div class="columns-3 m-partner anchor">
 				<a href="#" target="_blank"><img alt="Monarch Logo" src='<?php echo get_template_directory_uri("/"); ?>/img/Monarch_Horizontal_Grayscale_CMYK_familyofhotels@2x.png'></a>
 			</div>
-			<div class="columns-2 offset-by-1 m-partner desktop-only-show">
+			<div class="columns-3 m-partner desktop-only-show">
 				<img alt="Hampton Inn Logo" src='<?php echo get_template_directory_uri("/"); ?>/img/hamptoninn_gray.png'>
 			</div>
 			<div class="columns-3 m-partner middle desktop-only-show">
 				<img alt="Holiday Inn Logo" src='<?php echo get_template_directory_uri("/"); ?>/img/holidayinnsuites_withcharlestonwest.png'>
 			</div>
-			<div class="columns-2 m-partner desktop-only-show">
+			<div class="columns-3 m-partner desktop-only-show">
 				<img alt="Wingate Logo" src='<?php echo get_template_directory_uri("/"); ?>/img/wingate_gray_lighter.png'>
 			</div>
 			<div class="mobile-show">
